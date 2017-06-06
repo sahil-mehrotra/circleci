9cf3418a513ad6d9e25ff82de0787a12a1c3977e
mehrotras <msahil@thoughtworks.com>
2017-06-06 12:10:57 +0530

    calculator and unit test

---
+++ calculator.py
@@ -0,0 +1,8 @@
+
+class Calculator(object):
+
+    def add(self, number1, number2):
+        return number1 + number2
+
+    def subtract(self, param_one, param_two):
+        return param_one - param_two
---
+++ calculator_test.py
@@ -0,0 +1,16 @@
+import unittest
+from calculator import Calculator
+
+
+class CalculatorTest(unittest.TestCase):
+    def setUp(self):
+        self.calculator = Calculator()
+
+    def test_two_plus_three_equals_five(self):
+        self.assertEquals(5, self.calculator.add(2, 3))
+
+    def test_three_subtract_two_equals_one(self):
+        self.assertEquals(1, self.calculator.subtract(3, 2))
+
+    def test_two_subtract_three_equals_one(self):
+        self.assertEquals(-1, self.calculator.subtract(2, 3))

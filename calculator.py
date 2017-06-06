90eaada35bf2d7197893b053e733fc53d1372d06
mehrotras <msahil@thoughtworks.com>
2017-06-06 16:04:20 +0530

    fix to pass the test

--- calculator.py
+++ calculator.py
@@ -1,8 +1,13 @@
+import logging
+logging.basicConfig(filename='testLogging.log',level=logging.DEBUG)
+logging.debug('This message should go to the log file')
 
 class Calculator(object):
 
     def add(self, number1, number2):
+        logging.debug("params add {}, {}".format(number1, number2))
         return number1 + number2
 
     def subtract(self, param_one, param_two):
-        return param_one +gir param_two
+        logging.debug("params subtract {}, {}".format(param_one, param_two))
+        return param_one - param_two
